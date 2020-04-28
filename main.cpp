f48e22f6cf1fd56f561acf1147e6ed19484cd9f3
Youssef Walid <yousefwalid98@hotmail.com>
2020-04-28 12:02:09 +0200

    changed the way of reading files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include "AppManager.cpp"
+#include <iostream>
+#include <string> 
 
 int main()
 {
+	std::cout << "Enter the input file name without extension\n";
+	std::string fileName;
+	std::cin >> fileName;
+
 	AppManager app;
-	app.run("file.in", "file.bin", "Decode.out");
+	//app.run(fileName, "bin_" + fileName, fileName + ".out");
+	app.run((fileName+".in").c_str(), (fileName+".bin").c_str(), (fileName+".out").c_str());
 }
